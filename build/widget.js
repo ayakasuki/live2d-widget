0874573e716975cd7d5e5e59651e721a5232eff3
ayakasuki <27204037@163.com>
2025-06-11 15:42:16 +0800

    marge 1.0.0-rc4

--- build/widget.js
+++ build/widget.js
@@ -18,6 +18,17 @@ function registerEventListener(tips) {
     let userAction = false;
     let userActionTimer;
     const messageArray = tips.message.default;
+    tips.seasons.forEach(({ date, text }) => {
+        const now = new Date(), after = date.split('-')[0], before = date.split('-')[1] || after;
+        if (Number(after.split('/')[0]) <= now.getMonth() + 1 &&
+            now.getMonth() + 1 <= Number(before.split('/')[0]) &&
+            Number(after.split('/')[1]) <= now.getDate() &&
+            now.getDate() <= Number(before.split('/')[1])) {
+            text = randomSelection(text);
+            text = text.replace('{year}', String(now.getFullYear()));
+            messageArray.push(text);
+        }
+    });
     let lastHoverElement;
     window.addEventListener('mousemove', () => (userAction = true));
     window.addEventListener('keydown', () => (userAction = true));
@@ -33,7 +44,6 @@ function registerEventListener(tips) {
             }, 20000);
         }
     }, 1000);
-    showMessage(welcomeMessage(tips.time, tips.message.welcome, tips.message.referrer), 7000, 11);
     window.addEventListener('mouseover', (event) => {
         var _a;
         for (let { selector, text } of tips.mouseover) {
@@ -59,16 +69,13 @@ function registerEventListener(tips) {
             return;
         }
     });
-    tips.seasons.forEach(({ date, text }) => {
-        const now = new Date(), after = date.split('-')[0], before = date.split('-')[1] || after;
-        if (Number(after.split('/')[0]) <= now.getMonth() + 1 &&
-            now.getMonth() + 1 <= Number(before.split('/')[0]) &&
-            Number(after.split('/')[1]) <= now.getDate() &&
-            now.getDate() <= Number(before.split('/')[1])) {
-            text = randomSelection(text);
-            text = text.replace('{year}', String(now.getFullYear()));
-            messageArray.push(text);
-        }
+    window.addEventListener('live2d:hoverbody', () => {
+        const text = randomSelection(tips.message.hoverBody);
+        showMessage(text, 4000, 8, false);
+    });
+    window.addEventListener('live2d:tapbody', () => {
+        const text = randomSelection(tips.message.tapBody);
+        showMessage(text, 4000, 9);
     });
     const devtools = () => { };
     console.log('%c', devtools);
@@ -87,7 +94,7 @@ function loadWidget(config) {
     return __awaiter(this, void 0, void 0, function* () {
         var _a;
         localStorage.removeItem('waifu-display');
-        sessionStorage.removeItem('waifu-text');
+        sessionStorage.removeItem('waifu-message-priority');
         document.body.insertAdjacentHTML('beforeend', `<div id="waifu">
        <div id="waifu-tips"></div>
        <div id="waifu-canvas">
@@ -102,6 +109,7 @@ function loadWidget(config) {
             tips = yield response.json();
             models = tips.models;
             registerEventListener(tips);
+            showMessage(welcomeMessage(tips.time, tips.message.welcome, tips.message.referrer), 7000, 11);
         }
         const model = yield ModelManager.initCheck(config, models);
         yield model.loadModel('');
--- src/cubism2/index.js
+++ src/cubism2/index.js
@@ -1,9 +1,31 @@
-/* global document, window, Live2D */
+/* global document, window, Event, Live2D */
 import { L2DMatrix44, L2DTargetPoint, L2DViewMatrix } from './Live2DFramework.js';
 import LAppDefine from './LAppDefine.js';
 import MatrixStack from './utils/MatrixStack.js';
 import LAppLive2DManager from './LAppLive2DManager.js';
 import logger from '../logger.js';
+
+function normalizePoint(x, y, x0, y0, w, h) {
+  const dx = x - x0;
+  const dy = y - y0;
+
+  let targetX = 0, targetY = 0;
+
+  if (dx >= 0) {
+    targetX = dx / (w - x0);
+  } else {
+    targetX = dx / x0;
+  }
+  if (dy >= 0) {
+    targetY = dy / (h - y0);
+  } else {
+    targetY = dy / y0;
+  }
+  return {
+    vx: targetX,
+    vy: -targetY
+  };
+}
 
 class Cubism2Model {
   constructor() {
@@ -19,11 +41,7 @@ class Cubism2Model {
     this.projMatrix = null; /*new L2DMatrix44()*/
     this.deviceToScreen = null; /*new L2DMatrix44();*/
 
-    this.drag = false;
     this.oldLen = 0;
-
-    this.lastMouseX = 0;
-    this.lastMouseY = 0;
 
     this._boundMouseEvent = this.mouseEvent.bind(this);
     this._boundTouchEvent = this.touchEvent.bind(this);
@@ -36,11 +54,9 @@ class Cubism2Model {
       this.canvas.addEventListener('mousewheel', this._boundMouseEvent, false);
       this.canvas.addEventListener('click', this._boundMouseEvent, false);
 
-      this.canvas.addEventListener('mousedown', this._boundMouseEvent, false);
-      this.canvas.addEventListener('mousemove', this._boundMouseEvent, false);
+      document.addEventListener('mousemove', this._boundMouseEvent, false);
 
-      this.canvas.addEventListener('mouseup', this._boundMouseEvent, false);
-      this.canvas.addEventListener('mouseout', this._boundMouseEvent, false);
+      document.addEventListener('mouseout', this._boundMouseEvent, false);
       this.canvas.addEventListener('contextmenu', this._boundMouseEvent, false);
 
       this.canvas.addEventListener('touchstart', this._boundTouchEvent, false);
@@ -104,10 +120,8 @@ class Cubism2Model {
     if (this.canvas) {
       this.canvas.removeEventListener('mousewheel', this._boundMouseEvent, false);
       this.canvas.removeEventListener('click', this._boundMouseEvent, false);
-      this.canvas.removeEventListener('mousedown', this._boundMouseEvent, false);
-      this.canvas.removeEventListener('mousemove', this._boundMouseEvent, false);
-      this.canvas.removeEventListener('mouseup', this._boundMouseEvent, false);
-      this.canvas.removeEventListener('mouseout', this._boundMouseEvent, false);
+      document.removeEventListener('mousemove', this._boundMouseEvent, false);
+      document.removeEventListener('mouseout', this._boundMouseEvent, false);
       this.canvas.removeEventListener('contextmenu', this._boundMouseEvent, false);
 
       this.canvas.removeEventListener('touchstart', this._boundTouchEvent, false);
@@ -208,14 +222,9 @@ class Cubism2Model {
   }
 
   modelTurnHead(event) {
-    this.drag = true;
+    const rect = this.canvas.getBoundingClientRect();
 
-    const rect = event.target.getBoundingClientRect();
-
-    const sx = this.transformScreenX(event.clientX - rect.left);
-    const sy = this.transformScreenY(event.clientY - rect.top);
-    const vx = this.transformViewX(event.clientX - rect.left);
-    const vy = this.transformViewY(event.clientY - rect.top);
+    const { vx, vy } = normalizePoint(event.clientX, event.clientY, rect.left + rect.width / 2, rect.top + rect.height / 2, window.innerWidth, window.innerHeight);
 
     logger.trace(
       'onMouseDown device( x:' +
@@ -229,21 +238,18 @@ class Cubism2Model {
       ')',
     );
 
-    this.lastMouseX = sx;
-    this.lastMouseY = sy;
-
     this.dragMgr.setPoint(vx, vy);
-
     this.live2DMgr.tapEvent(vx, vy);
+
+    if (this.live2DMgr?.model.hitTest(LAppDefine.HIT_AREA_BODY, vx, vy)) {
+      window.dispatchEvent(new Event('live2d:tapbody'));
+    }
   }
 
   followPointer(event) {
     const rect = event.target.getBoundingClientRect();
 
-    const sx = this.transformScreenX(event.clientX - rect.left);
-    const sy = this.transformScreenY(event.clientY - rect.top);
-    const vx = this.transformViewX(event.clientX - rect.left);
-    const vy = this.transformViewY(event.clientY - rect.top);
+    const { vx, vy } = normalizePoint(event.clientX, event.clientY, rect.left + rect.width / 2, rect.top + rect.height / 2, window.innerWidth, window.innerHeight);
 
     logger.trace(
       'onMouseMove device( x:' +
@@ -257,19 +263,14 @@ class Cubism2Model {
       ')',
     );
 
-    if (this.drag) {
-      this.lastMouseX = sx;
-      this.lastMouseY = sy;
+    this.dragMgr.setPoint(vx, vy);
 
-      this.dragMgr.setPoint(vx, vy);
+    if (this.live2DMgr?.model.hitTest(LAppDefine.HIT_AREA_BODY, vx, vy)) {
+      window.dispatchEvent(new Event('live2d:hoverbody'));
     }
   }
 
   lookFront() {
-    if (this.drag) {
-      this.drag = false;
-    }
-
     this.dragMgr.setPoint(0, 0);
   }
 
@@ -277,27 +278,12 @@ class Cubism2Model {
     e.preventDefault();
 
     if (e.type == 'mousewheel') {
-      if (
-        e.clientX < 0 ||
-        this.canvas.clientWidth < e.clientX ||
-        e.clientY < 0 ||
-        this.canvas.clientHeight < e.clientY
-      ) {
-        return;
-      }
-
       if (e.wheelDelta > 0) this.modelScaling(1.1);
-      else this.modelScaling(0.9);
-    } else if (e.type == 'mousedown') {
-      if ('button' in e && e.button != 0) return;
-
+      else this.modelScaling(1);
+    } else if (e.type == 'click' || e.type == 'contextmenu') {
       this.modelTurnHead(e);
     } else if (e.type == 'mousemove') {
       this.followPointer(e);
-    } else if (e.type == 'mouseup') {
-      if ('button' in e && e.button != 0) return;
-
-      this.lookFront();
     } else if (e.type == 'mouseout') {
       this.lookFront();
     }
